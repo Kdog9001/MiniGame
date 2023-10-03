767c5c56e2583dff6e2fb596afe48d17cd035034
Kdog9001 <146856471+Kdog9001@users.noreply.github.com>
2023-10-03 11:55:13 -0400

    Changed Speed
    Changed the speed of the player and the spawn intervals and made the targets destroy themselves after 6 seconds

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -7,7 +7,7 @@ public class DetectCollisions : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 7);
     }
 
     // Update is called once per frame
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,7 +10,7 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 15;
     private float spawnPosZ = 15;
     private float startDelay = 2;
-    private float spawnInterval = 5f;
+    private float spawnInterval = 3f;
 
     // Start is called before the first frame update
     void Start()
