c1900330a780973fe9f3160a0777adcd92b043ad
Kdog9001 <146856471+Kdog9001@users.noreply.github.com>
2023-12-11 17:56:55 -0500

    Added Friendly Target
    Added a friendly target that if shot will deduct points from the player rather than add them
    
    12/11/23 - 30 min

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,15 +8,18 @@ public class SpawnManager : MonoBehaviour
     public GameObject Target;
     public GameObject HorizontalTarget;
     public GameObject GoldenTarget;
+    public GameObject FriendTarget;
     public GameManager ManagerObject;
     public bool Hoz;
     public bool gold;
+    private bool friend;
     //change to fit in with target boundaries that will be built
     private float spawnRangeX = 15;
     private float spawnPosZ = 15;
     private float startDelay = 2;
     private float spawnInterval = 3f;
     private float goldInterval = 15f;
+    private float friendInterval = 7f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +31,7 @@ public class SpawnManager : MonoBehaviour
         }
         Hoz = false;
         gold = false;
+        friend = false;
 
     }
 
@@ -43,6 +47,11 @@ public class SpawnManager : MonoBehaviour
         {
             gold = true;
             InvokeRepeating("SpawnGolTarget", startDelay, goldInterval);
+        }
+        if (friend == false && ManagerObject.score > 30)
+        {
+            friend = true;
+            InvokeRepeating("SpawnFriendTarget", startDelay, friendInterval);
         }
     }
 
@@ -69,6 +78,14 @@ public class SpawnManager : MonoBehaviour
         //random spawn location for targets
         Vector3 spawnPos = new Vector3(Random.Range(-12, spawnRangeX), 1.87f, spawnPosZ);
         Instantiate(GoldenTarget, spawnPos, Target.transform.rotation);
+        //Game Object ^               Spawn Location  ^rotation it spawns in at
+    }
+    void SpawnFriendTarget()
+    {
+        spawnPosZ = Random.Range(0, 15);
+        //random spawn location for targets
+        Vector3 spawnPos = new Vector3(Random.Range(-12, spawnRangeX), 1.87f, spawnPosZ);
+        Instantiate(FriendTarget, spawnPos, Target.transform.rotation);
         //Game Object ^               Spawn Location  ^rotation it spawns in at
     }
 
